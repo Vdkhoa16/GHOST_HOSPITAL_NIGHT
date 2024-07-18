254cdc141ec5e21dc70dca402c3bb234d29985cd
qkiet2k4 <104260937+qkiet1002@users.noreply.github.com>
2024-07-18 19:29:48 +0700

    Update Inventory Hand
    19/7/2024

--- Assets/Scripts/Inventory/PlayerInventory.cs
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using Unity.Netcode;
 using TMPro;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class PlayerInventory : NetworkBehaviour
 {
@@ -18,8 +19,33 @@ public class PlayerInventory : NetworkBehaviour
     [SerializeField] float pickupDistance;
     [SerializeField] KeyCode pickupButtom = KeyCode.E;
 
+
+    [Header("playerHandTransform")]
+    [SerializeField] private Transform playerHandTransform;
+    public GameObject currentItemInHand;
+
+    [System.Serializable]
+public struct ItemData : INetworkSerializable
+{
+    public string itemName;
+    public string prefabName;
+
+    public ItemData(Item item)
+    {
+        itemName = item.itemName;
+        prefabName = item.prefab.name;
+    }
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref itemName);
+        serializer.SerializeValue(ref prefabName);
+    }
+}
+
+
     [SerializeField] BoxCollider boxCollider;
-    [SerializeField] GameObject pickUpItem_gameobject;
+    private GameObject pickUpItem_gameobject;
     private bool isPlayerInRange = false;
     private NetworkObject networkObject;
     Transform worldObjectHolder;
@@ -82,7 +108,7 @@ public class PlayerInventory : NetworkBehaviour
     {
         if (isPlayerInRange && Input.GetKeyDown(pickupButtom))
         {
-            Debug.Log("Nhấn e");
+            //Debug.Log("Nhấn e");
             PickUp();
 
         }
@@ -90,6 +116,11 @@ public class PlayerInventory : NetworkBehaviour
         if (Input.GetKeyDown(inventoryButtom))
         {
             ToggleInventory();
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Debug.Log("Chuột Phải");
+            DropItem();
         }
     }
 
@@ -163,6 +194,8 @@ public class PlayerInventory : NetworkBehaviour
         }
     }
 
+
+
     void UpdateInventoryUI()
     {
         foreach (Transform child in invObjectHolder)
@@ -175,57 +208,135 @@ public class PlayerInventory : NetworkBehaviour
             GameObject obj = Instantiate(invCanvasObject, invObjectHolder);
             obj.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = invObj.item.itemName + " - " + invObj.amount;
             obj.transform.GetChild(1).GetComponent<Image>().sprite = invObj.item.itemImage;
-            obj.GetComponent<Button>().onClick.AddListener(delegate { DropItem(invObj.item); });
+            obj.GetComponent<Button>().onClick.AddListener(delegate { UseItem(invObj.item); });
         }
     }
 
-    void DropItem(Item item)
+    void MoveItemToHand(ItemData itemData)
+    {
+        // Tìm prefab từ Resources
+        GameObject prefab = Resources.Load<GameObject>(itemData.prefabName);
+        if (prefab != null)
+        {
+            GameObject itemObject = Instantiate(prefab, playerHandTransform.position, playerHandTransform.rotation, playerHandTransform);
+            itemObject.transform.SetParent(playerHandTransform);
+            itemObject.transform.localPosition = Vector3.zero; // Đặt vị trí local về 0,0,0 để gắn vào tay
+            itemObject.transform.localRotation = Quaternion.identity; // Đặt rotation local về 0 để gắn vào tay
+            Debug.Log("Đang sử dụng item: " + itemData.itemName);
+        }
+        else
+        {
+            Debug.LogError("Không tìm thấy prefab: " + itemData.prefabName);
+        }
+    }
+
+    void UseItem(Item item)
     {
         foreach (InventoryObject invObj in inventoryObjects)
         {
-            if (invObj.item != item)
+            if (invObj.item == item)
             {
-                continue;
-            }
-            if (invObj.amount > 1)
-            {
-                invObj.amount--;
-                DropItemServerRpc(invObj.item.prefab.name, boxCollider.transform.position + boxCollider.transform.forward);
-                Debug.Log(invObj.item.prefab.name);
-                UpdateInventoryUI();
-                return;
-            }
-            if (invObj.amount <= 1)
-            {
+                if (invObj.amount > 0)
+                {
+                    // Chuyển item thành ItemData và thông báo đến server
+                    ItemData itemData = new ItemData(item);
+                    MoveItemToHandServerRpc(itemData);
 
-                inventoryObjects.Remove(invObj);
-                DropItemServerRpc(invObj.item.prefab.name, boxCollider.transform.position + boxCollider.transform.forward);
-                Debug.Log(invObj.item.prefab.name);
-                UpdateInventoryUI();
-                return;
+                   // invObj.amount--;
+                    if (invObj.amount == 0)
+                    {
+                        inventoryObjects.Remove(invObj);
+                    }
+                    UpdateInventoryUI();
+                    return;
+                }
             }
         }
     }
+
     [ServerRpc(RequireOwnership = false)]
-    void DropItemServerRpc(string prefabName, Vector3 position)
+    void MoveItemToHandServerRpc(ItemData itemData)
     {
-        DropItemClientRpc(prefabName, position);
+        MoveItemToHandClientRpc(itemData);
+        MoveItemToHand(itemData);
     }
 
     [ClientRpc]
-    void DropItemClientRpc(string prefabName, Vector3 position)
+    void MoveItemToHandClientRpc(ItemData itemData)
     {
-        GameObject prefab = Resources.Load<GameObject>(prefabName);
-        if (prefab == null)
+        MoveItemToHand(itemData);
+    }
+
+    // DropItem
+    public void DropItem()
+    {
+        if (currentItemInHand == null)
         {
-            Debug.LogError("Failed to load prefab: " + prefabName);
+            Debug.LogWarning("Không có item nào đang cầm trên tay để drop.");
             return;
         }
 
-        GameObject drop = Instantiate(prefab, position, Quaternion.identity, worldObjectHolder);
-        drop.GetComponent<NetworkObject>().Spawn();
 
+        InventoryObject heldItem = inventoryObjects.Find(obj => obj.amount > 0);
+        if (heldItem == null)
+        {
+            Debug.LogWarning("Không có item nào đang cầm trên tay để drop.");
+            return;
+        }
+        heldItem.amount--;
+        if (heldItem.amount <= 0)
+        {
+            inventoryObjects.Remove(heldItem);
+        }
+        UpdateInventoryUI();
     }
+    /*void DropItem(Item item)
+      {
+          foreach (InventoryObject invObj in inventoryObjects)
+          {
+              if (invObj.item != item)
+              {
+                  continue;
+              }
+              if (invObj.amount > 1)
+              {
+                  invObj.amount--;
+                  DropItemServerRpc(invObj.item.prefab.name, boxCollider.transform.position + boxCollider.transform.forward);
+                  Debug.Log(invObj.item.prefab.name);
+                  UpdateInventoryUI();
+                  return;
+              }
+              if (invObj.amount <= 1)
+              {
+
+                  inventoryObjects.Remove(invObj);
+                  DropItemServerRpc(invObj.item.prefab.name, boxCollider.transform.position + boxCollider.transform.forward);
+                  Debug.Log(invObj.item.prefab.name);
+                  UpdateInventoryUI();
+                  return;
+              }
+          }
+      }
+      [ServerRpc(RequireOwnership = false)]
+      void DropItemServerRpc(string prefabName, Vector3 position)
+      {
+          DropItemClientRpc(prefabName, position);
+      }
+
+      [ClientRpc]
+      void DropItemClientRpc(string prefabName, Vector3 position)
+      {
+          GameObject prefab = Resources.Load<GameObject>(prefabName);
+          if (prefab == null)
+          {
+              Debug.LogError("Failed to load prefab: " + prefabName);
+              return;
+          }
+
+          GameObject drop = Instantiate(prefab, position, Quaternion.identity, worldObjectHolder);
+          drop.GetComponent<NetworkObject>().Spawn();
+
+      }*/
 
 
     [System.Serializable]
