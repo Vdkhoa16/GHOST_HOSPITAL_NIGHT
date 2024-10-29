65c8880ed07d087b22ba371fc9f620577577fcae
binhne123 <nbinf259@gmail.com>
2024-10-29 17:18:58 +0700

    29/10/2024

---
+++ Assets/Binh/prefab/Binh/Script/bed.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ObjectSoundTrigger : MonoBehaviour
+{
+    public AudioSource audioSource; // Âm thanh sẽ được phát khi lại gần đối tượng
+
+    private void Start()
+    {
+        audioSource.Stop(); // Đảm bảo âm thanh không phát khi bắt đầu
+    }
+
+    // Khi người chơi vào vùng của đối tượng
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Kiểm tra nếu người chơi (Player) vào vùng
+        {
+            if (!audioSource.isPlaying) // Chỉ phát nếu âm thanh chưa phát
+            {
+                audioSource.Play(); // Phát âm thanh khi lại gần
+            }
+        }
+    }
+
+    // Khi người chơi rời khỏi vùng của đối tượng
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // Kiểm tra nếu người chơi (Player) rời khỏi vùng
+        {
+            if (audioSource.isPlaying) // Chỉ tắt nếu âm thanh đang phát
+            {
+                audioSource.Stop(); // Tắt âm thanh khi rời xa
+            }
+        }
+    }
+}
