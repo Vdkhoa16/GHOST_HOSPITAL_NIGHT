1bb8bfd5ffead0237cad0bbeca4491e52e41b1f7
qkiet2k4 <104260937+qkiet1002@users.noreply.github.com>
2024-10-30 00:08:24 +0700

    Big Fix4

---
+++ Assets/Phong/Scripts/Mo_Ket_Sat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mo_Ket_Sat : MonoBehaviour
+{
+    [SerializeField] private Animator safeAnimator; // Tham chiếu đến Animator của két sắt
+    private bool isOpen = false; // Biến để theo dõi trạng thái mở của két sắt
+
+    public void OpenSafe()
+    {
+        if (!isOpen) // Kiểm tra xem két sắt đã mở chưa
+        {
+            isOpen = true; // Đánh dấu két sắt là đã mở
+            safeAnimator.SetBool("isOpen", true); // Kích hoạt trạng thái Open_Door
+        }
+    }
+}
