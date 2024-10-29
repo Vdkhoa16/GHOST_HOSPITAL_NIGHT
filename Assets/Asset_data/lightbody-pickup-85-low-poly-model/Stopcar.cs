791b3e8e05da4a592a0d26ba03a1f91c3960e8ed
qkiet2k4 <104260937+qkiet1002@users.noreply.github.com>
2024-10-29 23:59:14 +0700

    Big Fix 2

---
+++ Assets/Asset_data/lightbody-pickup-85-low-poly-model/Stopcar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stopcar : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartAni()
+    {
+        Debug.Log("STOP");
+        animator.SetBool("StopTrue",true);
+    }
+
+}
