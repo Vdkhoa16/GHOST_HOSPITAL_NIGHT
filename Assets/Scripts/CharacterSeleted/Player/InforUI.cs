e1316d18fe6e5306c2f4b69d374e43892145c0ea
qkiet2k4 <104260937+qkiet1002@users.noreply.github.com>
2024-10-31 21:52:04 +0700

    attributeManager
    31/10/24

---
+++ Assets/Scripts/CharacterSeleted/Player/InforUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InforUI : MonoBehaviour
+{
+    public TextMeshProUGUI hpUI;
+    public AttributesManager attributesManager;
+    //public override void OnNetworkSpawn()
+    //{
+    //    if (!IsOwner)
+    //    {
+    //        enabled = false;
+    //        return;
+    //    }
+    //    //if(NetworkManager.Singleton.LocalClient != null)
+    //    //{
+    //    //    var playerObject = NetworkManager.Singleton.LocalClient.PlayerObject;
+    //    //    if(playerObject != null)
+    //    //    {
+                 
+               
+    //    //    }
+    //    //}
+    //}
+
+    void Start()
+    {
+       // attributesManager = GetComponent<AttributesManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpUI.text = attributesManager.health.ToString();
+    }
+}
