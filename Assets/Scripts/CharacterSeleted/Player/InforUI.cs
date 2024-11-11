7803ac7c80414978dc54b217bcf5617ba237df0a
Võ Đăng Khoa <130233473+dangkhoa-vdkhoa@users.noreply.github.com>
2024-11-11 12:25:44 +0700

    up1

--- Assets/Scripts/CharacterSeleted/Player/InforUI.cs
+++ Assets/Scripts/CharacterSeleted/Player/InforUI.cs
@@ -1,38 +1,41 @@
-﻿    using System.Collections;
-    using System.Collections.Generic;
-    using TMPro;
-    using Unity.Netcode;
-    using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UIElements;
 
-    public class InforUI : MonoBehaviour
+public class InforUI : MonoBehaviour
+{
+    public TextMeshProUGUI hpUI;
+    public TextMeshProUGUI speedUI;
+    public TextMeshProUGUI liveUI;
+    private AttributesManager attributesManager;
+    void Start()
     {
-        public TextMeshProUGUI hpUI;
-        public TextMeshProUGUI speedUI;
-        private AttributesManager attributesManager;
+        attributesManager = GetComponentInParent<AttributesManager>();
 
-        void Start()
+        // Kiểm tra xem có phải là đối tượng của player cục bộ hay không
+        if (GetComponentInParent<NetworkObject>().IsOwner)
         {
-            attributesManager = GetComponentInParent<AttributesManager>();
-
-            // Kiểm tra xem có phải là đối tượng của player cục bộ hay không
-            if (GetComponentInParent<NetworkObject>().IsOwner)
-            {
-                // Hiển thị UI nếu là player cục bộ
-                this.gameObject.SetActive(true);
-            }
-            else
-            {
-                // Ẩn UI nếu không phải là player cục bộ
-                this.gameObject.SetActive(false);
-            }
+            // Hiển thị UI nếu là player cục bộ
+            this.gameObject.SetActive(true);
         }
-
-        void Update()
+        else
         {
-            if (attributesManager != null)
-            {
-                hpUI.text = "HP: " + attributesManager.health.ToString();
-                speedUI.text = "Speed: " + attributesManager.running_Speed.ToString();
-            }
+            // Ẩn UI nếu không phải là player cục bộ
+            this.gameObject.SetActive(false);
         }
     }
+
+    void Update()
+    {
+        if (attributesManager != null)
+        {
+            hpUI.text = "HP: " + attributesManager.health.ToString();
+            speedUI.text = "Speed: " + attributesManager.running_Speed.ToString();
+            liveUI.text = "Live: "+ attributesManager.live.ToString();
+        }
+    }
+}
