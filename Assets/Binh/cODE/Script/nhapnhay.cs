d40a2846efa4f0ae8ff78cc9ac7450520ba3b665
binhne123 <nbinf259@gmail.com>
2024-11-26 02:20:43 +0700

    26/11

---
+++ Assets/Binh/cODE/Script/nhapnhay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BlinkWithAlpha : MonoBehaviour
+{
+    public Renderer targetRenderer; // Renderer của object
+    public float blinkSpeed = 2f; // Tốc độ nhấp nháy
+
+    private Material material;
+    private Color originalColor;
+
+    private void Start()
+    {
+        material = targetRenderer.material;
+        originalColor = material.color;
+    }
+
+    private void Update()
+    {
+        float alpha = Mathf.Abs(Mathf.Sin(Time.time * blinkSpeed)); // Tạo hiệu ứng nhấp nháy
+        material.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+    }
+}
